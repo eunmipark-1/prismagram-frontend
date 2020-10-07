29894433f69e4b207b3ac4c433e2f0c8b00abd98
박은미 <mia1221@kakao.com>
2020-10-07 23:59:39 +0900

    apollo client 기본 설정

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
+import { ApolloProvider } from 'react-apollo-hooks';
 import Client from './Apollo/Client';
-import {ApolloProvider} from '@apollo/react-hooks';
 
 ReactDOM.render(
   <ApolloProvider client={Client}>
